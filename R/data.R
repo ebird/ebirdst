75e9b9f811c4fe532f171f2154a716c038b9c214
Matt Strimas-Mackey <mes335@cornell.edu>
2023-11-07 15:13:28 -0500

    documenting datasets

--- R/data.R
+++ R/data.R
@@ -90,5 +90,14 @@
 "ebirdst_predictors"
 
 #' eBird Status and Trends predictors datasets
+#'
+#' Details on the datasets used to generate the eBird Status and Trends
+#' predictor variables.
+#'
+#' @format A data frame with 37 rows and 4 columns
+#' - `dataset`: dataset name.
+#' - `predictor`: predictor name or, if multiple variables are derived from
+#' this dataset, the pattern used to generate the names.
+#' - `description`: detailed description of the dataset or variable.
+#' - `reference`: a reference to consult for further information on the dataset.
 "ebirdst_predictor_datasets"
-
